678daec90d4124c4ce961e20bf3022487540f415
Leandro <serzoleandro@gmail.com>
2018-03-01 15:03:33 +0800

    Update on model.php, database and bootstrap.php.
    1. Delete old code in model.php.
    2. Create database.php and add PDO connection.
    3. Add DB details in bootstrap.php.

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -6,6 +6,11 @@ include("controllers/indexController.php");
 include("controllers/fakeController.php");
 
 define("BASE_URL", "/sims");
+define('DB_HOST','localhost');
+define('DB_USER','root');
+define('DB_PASS','^#XKL9f?Hn8NkyAH');
+define('DB_NAME','sims');
+
 $indexController = new IndexController();
 
 Router::$defaultController = $indexController;
